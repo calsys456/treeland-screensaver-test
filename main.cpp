2951bd9a9470e4f1b4f59559c4f2267a2a1c69ac
root <root@april-deepin-utm-apple.tail5443c1.ts.net>
2026-03-04 16:32:14 +0800

    Update

--- main.cpp
+++ main.cpp
@@ -8,18 +8,17 @@ int main(int argc, char *argv[])
 {
     system("pkill -f /usr/libexec/treeland-screensaver"); // Ensure no previous instance is running
     const QString busName("treeland-screensaver-test");
+    auto conn = QDBusConnection::connectToBus(QDBusConnection::SessionBus, busName);
+    QDBusInterface interface("org.freedesktop.ScreenSaver",
+                             "/org/freedesktop/ScreenSaver",
+                             "org.freedesktop.ScreenSaver",
+                             conn);
+    if (interface.isValid())
+        qInfo() << "Connected to org.freedesktop.ScreenSaver.";
+    else
+        qFatal() << "Failed to connect to org.freedesktop.ScreenSaver:"
+                 << QDBusConnection::sessionBus().lastError().message();
     {
-        auto conn = QDBusConnection::connectToBus(QDBusConnection::SessionBus, busName);
-        QDBusInterface interface("org.freedesktop.ScreenSaver",
-                                 "/org/freedesktop/ScreenSaver",
-                                 "org.freedesktop.ScreenSaver",
-                                 conn);
-        if (interface.isValid())
-            qInfo() << "Connected to org.freedesktop.ScreenSaver.";
-        else
-            qFatal() << "Failed to connect to org.freedesktop.ScreenSaver:"
-                     << QDBusConnection::sessionBus().lastError().message();
-
         QDBusReply<uint> reply1 = interface.call("Inhibit", "treeland-screensaver-test", "Testing inhibit/uninhibit");
         uint cookie;
         if (reply1.isValid()) {
@@ -36,13 +35,14 @@ int main(int argc, char *argv[])
             qFatal() << "Failed to UnInhibit cookie 1:" << reply2.error().message();
         }
 
-        sleep(0.1);
-        if (system("pgrep -fx /usr/libexec/treeland-screensaver > /dev/null") != 0) {
-            qInfo() << "treeland-screensaver has exited as expected.";
-        } else {
-            system("ps -p $(pgrep -fx /usr/libexec/treeland-screensaver)");
-            qFatal() << "treeland-screensaver is still running unexpectedly.";
-        }
+        // Commented since https://github.com/linuxdeepin/treeland/commit/b9b3a885c4a2ce22ac5171e1c69e9a6730a255e1
+        // sleep(0.1);
+        // if (system("pgrep -fx /usr/libexec/treeland-screensaver > /dev/null") != 0) {
+        //     qInfo() << "treeland-screensaver has exited as expected.";
+        // } else {
+        //     system("ps -p $(pgrep -fx /usr/libexec/treeland-screensaver)");
+        //     qFatal() << "treeland-screensaver is still running unexpectedly.";
+        // }
 
         QDBusReply<uint> reply3 = interface.call("Inhibit", "treeland-screensaver-test", "Testing inhibit/uninhibit");
         uint cookie1;
@@ -69,15 +69,27 @@ int main(int argc, char *argv[])
             qFatal() << "Failed to UnInhibit cookie 1:" << reply5.error().message();
         }
     }
-    qInfo() << "Disconnecting from D-Bus to test automatic uninhibit...";
-    QDBusConnection::disconnectFromBus(busName); // Simulate client disconnection
-    sleep(0.1); // Wait a moment for the screensaver to exit
-    if (system("pgrep -fx /usr/libexec/treeland-screensaver > /dev/null") != 0) {
-        qInfo() << "treeland-screensaver has exited as expected.";
+    // Commented since https://github.com/linuxdeepin/treeland/commit/b9b3a885c4a2ce22ac5171e1c69e9a6730a255e1
+    // qInfo() << "Disconnecting from D-Bus to test automatic uninhibit...";
+    // QDBusConnection::disconnectFromBus(busName); // Simulate client disconnection
+    // sleep(0.1); // Wait a moment for the screensaver to exit
+    // if (system("pgrep -fx /usr/libexec/treeland-screensaver > /dev/null") != 0) {
+    //     qInfo() << "treeland-screensaver has exited as expected.";
+    // } else {
+    //     system("ps -p $(pgrep -fx /usr/libexec/treeland-screensaver)");
+    //     qFatal() << "treeland-screensaver is still running unexpectedly.";
+    // }
+
+    qInfo() << "Basic test Success. Now we start an inhibit and sleep for 100 seconds, and you can use test_idle_inhibit_v1 to test if idle inhibit is exactly working or not.";
+
+    QDBusReply<uint> reply = interface.call("Inhibit", "treeland-screensaver-test", "Testing inhibit/uninhibit");
+    uint cookie;
+    if (reply.isValid()) {
+        cookie = reply.value();
+        qInfo() << "Inhibited. Cookie:" << cookie;
     } else {
-        system("ps -p $(pgrep -fx /usr/libexec/treeland-screensaver)");
-        qFatal() << "treeland-screensaver is still running unexpectedly.";
+        qFatal() << "Failed to call Inhibit:" << reply.error().message();
     }
-    qInfo() << "Success.";
+    sleep(100); // Sleep for a while to allow manual testing of idle inhibit
     return 0;
 }
